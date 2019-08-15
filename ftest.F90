e3900b0067bedbd8a59cc19c80e34bdc0976c740
Michel Valin <mfvalin@gmail.com>
2019-08-15 18:37:18 -0400

    print test statuses

--- ftest.F90
+++ ftest.F90
@@ -14,8 +14,11 @@ program test
   hash = fasthash64(C_LOC(str(1:1)), 32)
   print 100,'hash of '//trim(str)//' =',hash
   status = hash_insert(context, C_LOC(str(1:1)), len(trim(str)), C_LOC(data(1)), 512, 1)
+  if(status == 0) print 100,'SUCCESS'
   status = hash_insert(context, C_LOC(str(1:1)), len(trim(str)), C_LOC(data(1)), 512, 1)
+  if(status /= 0) print 100,'DUPLICATE'
   status = hash_insert(context, C_LOC(str(1:1)), len(trim(str)), C_LOC(data(1)), 512, 1)
+  if(status /= 0) print 100,'DUPLICATE'
 
   call print_hash_context_stats(context, "Blah Blah Blah"//achar(0))
 
